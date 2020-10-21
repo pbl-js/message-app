1eb1429f5217645ecde58f62175e1654c5925ec2
Paweł Miłczak <jogobello4@gmail.com>
2020-10-21 20:29:33 +0200

    Connect apollo client with phoenix

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,5 +1,11 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { split } from "apollo-link";
 import { setContext } from "@apollo/client/link/context";
+
+import { Socket as PhoenixSocket } from "phoenix";
+import * as AbsintheSocket from "@absinthe/socket";
+import { createAbsintheSocketLink } from "@absinthe/socket-apollo-link";
+import { hasSubscription } from "@jumpn/utils-graphql";
 
 import config from "../../config";
 
@@ -16,8 +22,26 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const authedHttpLink = authLink.concat(httpLink);
+
+const phoenixSocket = new PhoenixSocket(config.ws, {
+  params: {
+    token: config.token,
+  },
+});
+
+const absintheSocket = AbsintheSocket.create(phoenixSocket);
+
+const websocketLink = createAbsintheSocketLink(absintheSocket);
+
+const link = split(
+  (operation) => hasSubscription(operation.query),
+  websocketLink,
+  authedHttpLink
+);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: link,
   cache: new InMemoryCache(),
 });
 
